645e250ab836a24c13b6637b3b1ea9aa47bf5b70
Brian Johnson <brian@clickfunnels.com>
2017-06-09 15:46:11 -0400

    Create test_bjwebcalc.py

--- test_bjwebcalc.py
+++ test_bjwebcalc.py
@@ -14,7 +14,6 @@ def pattern():
                 pattern="{{ a * b }}"
             )
         )
-
 @pytest.fixture
 def client():
     return app.test_client()
@@ -27,16 +26,9 @@ def describe_index():
 
         expect(response.data).contains(b"Hello, world!")
 
-def describe_calc():
-
-        def when_plus(client):
-            response = client.get('/4/+/5')
-
-            expect(response.data).contains(b"9")
 
 
         def from_db(client, pattern):
             response = client.get('/4/x/5')
 
             expect(response.data).contains(b"20")
-
