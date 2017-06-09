e52a2ccb3cb6e6f29e804bdc0fbd848c107a7dfc
Brian Johnson <brian@clickfunnels.com>
2017-06-09 14:42:41 -0400

    slide 59

--- bjwebcalc.py
+++ bjwebcalc.py
@@ -1,8 +1,16 @@
+import os
+
 from flask import Flask
+from flask_pymongo import PyMongo
+from jinja2 import Template
 
 
 app = Flask(__name__)
 
+if os.getenv('MONGODB_URI'):
+    app.config['MONGO_URI'] = os.getenv('MONGODB_URI')
+
+mongo = PyMongo(app)
 
 @app.route('/')
 def index():
